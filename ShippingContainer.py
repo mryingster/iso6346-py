24676542dfefd9478e90fe6f1e4af3c37124c374
Michael Caldwell <michaelc@yingster.net>
2014-05-17 16:38:27 -0700

    Adding 'die' funciton for erros. Little bit of cleaning.

--- ShippingContainer.py
+++ ShippingContainer.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
-
 import re
+
+def die(message):
+    print message
+    quit()
 
 ShippingNumber = raw_input("Enter Container Code (eg. PSSU210948): ")
 
+# Check Number Format
 if not re.compile('\D{4}\d{6}').match(ShippingNumber):
-    print "Invalid, please ensure code follows ISO 6346 specifications."
-    quit()
+    die("Invalid, please ensure code follows ISO 6346 specifications.")
 
+# Calculate Checksum
 Total=0
-
 for i in range(len(ShippingNumber)):
     Value=ShippingNumber[i]
     if not Value.isdigit():
