1c46bd3f5c5c42949d3f89467a036efff9cd8f8d
Michael Caldwell <michaelc@yingster.net>
2014-05-17 17:12:09 -0700

    Splitting out functions for format check and checksum calculation

--- ShippingContainer.py
+++ ShippingContainer.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python
-import re
+import sys
 
 def die(message):
     print message
     quit()
 
-ShippingNumber = raw_input("Enter Container Code (eg. PSSU210948): ")
+def checkFormat(number):
+    import re
+    if re.compile('\D{4}\d{6}').match(number):
+        return
+    else:
+        die("Invalid, please ensure code follows ISO 6346 specifications.")
 
-# Check Number Format
-if not re.compile('\D{4}\d{6}').match(ShippingNumber):
-    die("Invalid, please ensure code follows ISO 6346 specifications.")
+def calculateSum(number):
+    Total=0
+    for i in range(len(number)):
+        Value=number[i]
+        if not Value.isdigit():
+            Value=ord(Value.lower())-ord('a')+10
+            Value+=Value/11
+            Total+=int(Value)*2**i
+    return Total % 11 % 10
 
-# Calculate Checksum
-Total=0
-for i in range(len(ShippingNumber)):
-    Value=ShippingNumber[i]
-    if not Value.isdigit():
-        Value=ord(Value.lower())-ord('a')+10
-        Value+=Value/11
-        Total+=int(Value)*2**i
+if len(sys.argv) < 2:
+    number = raw_input("Enter Container Code (eg. PSSU210948): ")
 
-print "Check Digit:", str(Total%11%10)
+checkFormat(number)
+print "Check Digit:", calculateSum(number)
+
